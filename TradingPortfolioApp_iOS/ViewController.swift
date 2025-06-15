9b7e0f7109f3c01df3bdf9ec3079344cd0bef8a2
ratzr15 <rathishnk@hotmail.co.in>
2025-06-15 22:45:31 +0400

    Initial Commit

---
+++ TradingPortfolioApp_iOS/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TradingPortfolioApp_iOS
+//
+//  Created by Rathish on 15/06/2025.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
